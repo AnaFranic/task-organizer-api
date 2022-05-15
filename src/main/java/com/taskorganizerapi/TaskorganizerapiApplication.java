a7d0d8c2ed6b939cdac9b5c0b91de234e6952c82
AnaFranic <anafranic@outlook.com>
2022-05-15 20:04:59 +0200

    Added spring boot initializr project

---
+++ src/main/java/com/taskorganizerapi/TaskorganizerapiApplication.java
@@ -0,0 +1,13 @@
+package com.taskorganizerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskorganizerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskorganizerapiApplication.class, args);
+	}
+
+}
